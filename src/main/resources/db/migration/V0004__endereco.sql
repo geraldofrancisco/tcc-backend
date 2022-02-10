17fd3676e16835e6a3ac2e2ef810ecb925d4d1cf
Geraldo Francisco <geraldo.francisco@foton.la>
2022-02-10 17:43:54 -0300

    migração de endereço

---
+++ src/main/resources/db/migration/V0004__endereco.sql
@@ -0,0 +1,18 @@
+create table thor.endereco
+(
+    id          bigserial    not null primary key,
+    usuario_id  bigint       not null,
+    cep         varchar(10)  not null,
+    cidade_id   integer      not null,
+    logradouro  varchar(100) not null,
+    numero      smallint,
+    complemento varchar(50)
+);
+
+alter table thor.endereco
+    add constraint fk_endereco_usuario foreign key (usuario_id)
+        references thor.usuario (id);
+
+alter table thor.endereco
+    add constraint fk_endereco_cidade foreign key (cidade_id)
+        references thor.cidade (id);
