5229c653f73971ab3ebc0d4193d67d98c3b3c757
Geraldo Francisco <geraldo.francisco@foton.la>
2022-02-12 22:40:16 -0300

    ajustes de mapeamento do endpoint dos correios

--- src/main/java/com/thor/fitness/dto/correio/CorreioDTO.java
+++ src/main/java/com/thor/fitness/dto/correio/CorreioDTO.java
@@ -1,11 +1,27 @@
 package com.thor.fitness.dto.correio;
 
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
 @Data
 public class CorreioDTO {
     private String bairro;
     private String cep;
+    private String logradouro;
     private String cidade;
-    protected String uf;
+    private String uf;
+
+    public String getBairro() {
+        if(StringUtils.isBlank(this.bairro)) {
+            return null;
+        }
+        return this.bairro;
+    }
+
+    public String getLogradouro() {
+        if(StringUtils.isBlank(this.logradouro)) {
+            return null;
+        }
+        return this.logradouro;
+    }
 }
--- src/main/java/com/thor/fitness/mapper/CorreioMapper.java
+++ src/main/java/com/thor/fitness/mapper/CorreioMapper.java
@@ -3,8 +3,10 @@ package com.thor.fitness.mapper;
 import com.thor.fitness.dto.correio.CorreioDTO;
 import com.thor.fitness.ws.EnderecoERP;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CorreioMapper {
+    @Mapping(target = "logradouro", source = "end")
     CorreioDTO toDTO(EnderecoERP dto);
 }
