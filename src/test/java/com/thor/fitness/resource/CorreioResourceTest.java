387a10721bf62045cafbbff30e3782ce0711a5eb
Geraldo Francisco <geraldo.francisco@foton.la>
2022-02-20 14:39:01 -0300

    teste unitário do endpoint de correio

---
+++ src/test/java/com/thor/fitness/resource/CorreioResourceTest.java
@@ -0,0 +1,54 @@
+package com.thor.fitness.resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class CorreioResourceTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void deveResponder200CepValido() throws Exception {
+        URI uri = new URI("/correio/cep/31235100");
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .get(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(HttpStatus.OK.value())
+                );
+    }
+
+    @Test
+    public void deveResponder400CepInvalido() throws Exception {
+        URI uri = new URI("/correio/cep/31313131");
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .get(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .is(HttpStatus.BAD_REQUEST.value())
+                );
+    }
+}
